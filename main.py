ae7b2c2f3f0e5372fb76db033426fd802ee4ab97
Mohamed Hamdi <offwitt0@gmail.com>
2025-07-14 16:11:42 +0300

    MIGRATION 11

--- main.py
+++ main.py
@@ -181,3 +181,7 @@ fastapi_app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:fastapi_app", host="0.0.0.0", port=8000)
