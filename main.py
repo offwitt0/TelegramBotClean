9c0f8a91185a91175112ebfa2dea80fa4367c56f
tonaja <tonaja.mohamed@gmail.com>
2025-07-21 13:09:55 +0100

    try again

--- main.py
+++ main.py
@@ -26,36 +26,8 @@ from openai import OpenAI
 #hello ive changed this here
 #hello im trying to push again
 # Payment
-import requests
  
-# API endpoint
-url = "https://subscriptionsmanagement-dev.fastautomate.com/api/Payments/reservation"
-# Data payload
-data = {
-    "userName": "tonaja Mohamed",
-    "email": email,
-    "roomType": "test",
-    "checkIn": "2025-07-17T12:39:40.090Z",
-    "checkOut": "2025-07-17T12:39:40.091Z",
-    "numberOfGuests": 3,
-    "amountInCents": 7000,
-    "successfulURL": "http://localhost:3000/thanks",
-    "cancelURL": "http://localhost:3000/cancel"
-}
-# Send the POST request
-response = requests.post(url, json=data)
- 
-def Payment():
-    # Check the response
-    if response.status_code == 200:
-        result = response.json()
-        PaymentUrl = result['sessionURL']
-        sessionStatus = result['sessionStatus']
-        return result
-    else:
-        failed = 'failed to send'
-        return failed
- 
+
 # ================== ENV & CONFIG ==================
 load_dotenv()
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
